36f5763ec70db4196c3a638bd3848732a8835ad6
JEAROM9416 <jearom.9419@gmail.com>
2018-10-21 09:32:32 -0600

    Commit

--- ASTSpaceApps/src/app/app.js
+++ ASTSpaceApps/src/app/app.js
@@ -4,23 +4,9 @@
 const natGeoKey = '3a03e0800e99420783f38f0556fc65ee'
 const googlekey = 'AIzaSyAW8pQBqAxchOUPXkt41Yvi2RGZE6rF2Hs'
 const request = require('request');
-/*
-request({
-    url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAveWVLoYq6ljCkofdltKYYRy6FLnEw9As',
-    json: true
-}, (error, response, body) => {
-    console.log(JSON.stringify(body,undefined,3));
-});*/
-/*
-request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyAW8pQBqAxchOUPXkt41Yvi2RGZE6rF2Hs ',
-    json: true
-}, (error, response, body) => {
-    console.log(JSON.stringify(body,undefined,3));
-});*/
 
 /** GEOCODING API */
-function getPlace(longitud, latitude) {
+function getPlaceByLanLat(longitud, latitude) {
     request({
         url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+longitud+','+latitude+'&key='+googlekey,
         json: true
@@ -28,4 +14,16 @@ function getPlace(longitud, latitude) {
         console.log(JSON.stringify(body,undefined,2));
     });
 }
-getPlace(40.714224,-73.961452);
+
+//getPlaceByLanLat(40.714224,-73.961452);
+
+function getPlaceByName(placename) {
+    request({
+        url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input='+placename+'&key='+googlekey,
+        json: true
+    }, (error, response, body) => {
+        console.log(JSON.stringify(body,undefined,2));
+    });
+}
+
+getPlaceByName('Tecnológico de Costa Rica')
